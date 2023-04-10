25451342f27f7b10ff3a6426a7a4f279e56ff9eb
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-04-10 14:48:34 -0300

    aula de variavies python ada cursos

---
+++ variaveis.py
@@ -0,0 +1,28 @@
+idade = 26 # criando uma variável  
+print(idade)
+
+nome = 'Aline Melo'
+print(nome)
+
+""" Tipos de variável 
+1: int: números inteiros, ou seja, números sem parte decimal: 0, 5, -1, 1000
+2: float: números reais, ou seja, números com parte decimal: -2.7, 3.14
+3: str: cadeias de caracteres, ou seja, dados textuais
+4: bool: valores lógicos (booleanos) True ou False
+"""
+
+idade = 26 #int
+altura = 1.60 #float
+nome = 'Aline Melo' #str
+estudando = True #bool
+
+print(type(idade))
+print(type(altura))
+print(type(nome))
+print(type(estudando))
+
+#obtendo dados do usuário e salvando em variáveis
+
+linguagem = input('Qual é a linguagem de programação que você está estudando?')
+
+print('A linguagem que você está estudando é', linguagem) 
