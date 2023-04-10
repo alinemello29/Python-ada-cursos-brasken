6d3ac5110c4da50fe0ccd20708b24d2d68b4802c
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-04-10 16:19:29 -0300

    aula de operadores aritméticos e booleanos|ada cursos

---
+++ operadores.py
@@ -0,0 +1,38 @@
+# > Operações matemáticas (aritméticas)
+
+""" 
+- soma: +
+- subtração: -
+- multiplicação: *
+- divisão: /
+- divisão inteiros: //
+- resto da divisão: %
+- potência: **
+"""
+
+numero1 = 10 
+numero2 = 20
+
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2)
+print(20 % 3)
+print(2 ** 3)
+
+#operadores booleanos 
+
+idade1 = 10
+idade2 = 20
+altura1 = 1.62
+altura2 = 1.69
+altura3 = altura2
+
+
+print(idade1 > idade2)
+print(idade1 <= idade2)
+print('python' == 'python')
+print('banana' != 'abacaxi')
+print('altura1' >= 'altura2')
+print(altura2 >= altura3)
